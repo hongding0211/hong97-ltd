43e64367c7e8ac3416ad36997784aadb336d9c8d
hongding0211 <keith.dh@hotmail.com>
2022-11-21 16:43:43 +0800

    feat: mvp

--- components/appLayout/appLayout.tsx
+++ components/appLayout/appLayout.tsx
@@ -2,10 +2,11 @@ import React, {useState} from "react"
 import {IMenuItem} from "../../config/config";
 import Logo from "../logo";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faEllipsisVertical} from "@fortawesome/free-solid-svg-icons";
+import {faEllipsisVertical, faEnvelope, faXmark} from "@fortawesome/free-solid-svg-icons";
 import Divider from "../divider";
 import NavButtons from "./navButtons";
 import useDarkMode from 'use-dark-mode'
+import {faWeixin} from "@fortawesome/free-brands-svg-icons"
 
 interface IAppLayout {
   menuConfig: IMenuItem[]
@@ -35,10 +36,10 @@ const AppLayout: React.FC<IAppLayout> = props => {
   return (
     <div className='text-neutral-900 dark:text-neutral-50'>
       <nav
-        className='h-[64px] flex items-center justify-between px-5 sticky top-0 backdrop-blur-xl backdrop-saturate-150 bg-white/[0.7] dark:bg-black/[0.7] backdrop-brightness-125'>
+        className='h-[64px] flex items-center justify-between px-5 sticky top-0 backdrop-blur-xl backdrop-saturate-150 bg-white/[0.5] dark:bg-black/[0.5]'>
         <Logo width={20} className='fill-neutral-800 dark:fill-neutral-100'/>
         <FontAwesomeIcon
-          icon={faEllipsisVertical}
+          icon={showMenu ? faXmark : faEllipsisVertical}
           className='h-[20px] px-3 cursor-pointer hover:scale-110 transition-transform duration-150 ease-in-out sm:hidden'
           onClick={() => setShowMenu(!showMenu)}
         />
@@ -57,7 +58,7 @@ const AppLayout: React.FC<IAppLayout> = props => {
       </nav>
       {
         showMenu &&
-        (<div className='h-[calc(100vh-64px)] sm:hidden'>
+        (<div className='sm:hidden fixed top-[64px] left-0 right-0 bottom-0 bg-white/[0.5] dark:bg-black/[0.5] z-10 backdrop-blur-xl backdrop-saturate-150'>
           <Divider/>
           <div className='grid grid-cols-1 mx-5 divide-y divide-neutral-600 dark:divide-neutral-300'>
             {props.menuConfig.map(m => (
@@ -71,6 +72,50 @@ const AppLayout: React.FC<IAppLayout> = props => {
           </div>
         </div>)
       }
+
+      <div className='min-h-[calc(100vh-64px)] relative'>
+
+        <footer className='absolute bg-neutral-100 dark:bg-neutral-900 bottom-0 w-full p-5 text-xs font-light text-neutral-400 dark:text-neutral-500'>
+          <div className='flex flex-col sm:flex-row gap-x-10 gap-y-3'>
+            <div className='flex flex-col gap-y-1'>
+              <span className='font-medium text-neutral-500 dark:text-neutral-400'>联系我</span>
+              <span><FontAwesomeIcon icon={faEnvelope} className='mr-1 text-[10px]'/>keith.dh@hotmail.com</span>
+              <span><FontAwesomeIcon icon={faWeixin} className='mr-1 text-[10px]'/>1479224723</span>
+            </div>
+
+            <div className='flex flex-col gap-y-1'>
+              <span className='font-medium text-neutral-500 dark:text-neutral-400'>我的项目</span>
+              <a
+                href='https://hong97.ltd/walkingcalc/'
+                target='_blank' rel="noreferrer"
+                className='text-sky-700 dark:text-sky-600 hover:underline'
+              >Walking Calculator</a>
+              <a
+                href='https://hong97.ltd/sso/login'
+                target='_blank' rel="noreferrer"
+                className='text-sky-700 dark:text-sky-600 hover:underline'
+              >Single Sign On</a>
+            </div>
+          </div>
+
+          <div className='h-[1px] bg-neutral-300 dark:bg-neutral-600 my-3'/>
+
+          <div className='flex gap-x-2 gap-y-1 flex-wrap'>
+            <span>Copyright © {new Date().getFullYear()} hong97.ltd. </span>
+            <a
+              href='https://github.com/hongding0211/hong97-ltd'
+              target='_blank' rel="noreferrer"
+              className='hover:underline'
+            >Github Repo</a>
+            <a
+              href='https://beian.miit.gov.cn/#/Integrated/index'
+              target='_blank' rel="noreferrer"
+              className='hover:underline'
+            >沪 ICP 备 2022003448 号
+            </a>
+          </div>
+        </footer>
+      </div>
     </div>
   )
 }
