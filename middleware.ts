3f761cc1159526a4c2f2833bf59ea3da8a916106
hongding0211 <keith.dh@hotmail.com>
2022-11-23 01:17:54 +0800

    feat: add log

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,7 @@ export function middleware(request: NextRequest) {
   const url = request.nextUrl.clone()
   // TODO 暂时重定向到 about 页面
   if (url.pathname === '/') {
+    console.log(new Date(), request.headers.get('user-agent'))
     url.pathname = '/about'
     return NextResponse.redirect(url)
   }
